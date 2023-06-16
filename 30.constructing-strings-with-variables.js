8924f874cb3e15ba82a84ea18c5c36324cad5300
McWolf <lobezno2001@gmail.com>
2023-06-16 21:06:01 +0200

    exercise 30

--- 30.constructing-strings-with-variables.js
+++ 30.constructing-strings-with-variables.js
@@ -1,3 +1,3 @@
-// Cambia solo el código debajo de esta línea
+// Construye cadenas con variables
 const myName = "McWolvie";
 const myStr = "hello there, my name is " + myName + " , how do you do";
