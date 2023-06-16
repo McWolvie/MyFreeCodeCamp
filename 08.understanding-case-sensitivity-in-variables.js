24196194a1117339632ab39549178bdcdf62e1b1
McWolf <lobezno2001@gmail.com>
2023-06-16 20:29:30 +0200

    exercise 08

--- 08.understanding-case-sensitivity-in-variables.js
+++ 08.understanding-case-sensitivity-in-variables.js
@@ -1,9 +1,9 @@
-// Declaración de variables
+// Comprendiendo la sensibilidad de mayúsculas en las variables
 var studlyCapVar;
 var properCamelCase;
 var titleCaseOver;
 
-// Asignación de variables
+
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
