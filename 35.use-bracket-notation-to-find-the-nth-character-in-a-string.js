b7a5908e0f56e3b5f3171e3b030f16b74b338a81
McWolf <lobezno2001@gmail.com>
2023-06-16 21:13:53 +0200

    exercise 35

--- 35.use-bracket-notation-to-find-the-nth-character-in-a-string.js
+++ 35.use-bracket-notation-to-find-the-nth-character-in-a-string.js
@@ -1,5 +1,4 @@
-// Configuración
+// Utiliza la notación de corchetes para encontrar el enésimo carácter en una cadena
 const lastName = "Lovelace";
 
-// Cambia solo el código debajo de esta línea
-const thirdLetterOfLastName = lastName[2]; // Cambia esta línea
+const thirdLetterOfLastName = lastName[2]; 
