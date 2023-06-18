35d40c66f0052e5ac2be86d2be7701c3c6091883
McWolf <lobezno2001@gmail.com>
2023-06-18 13:17:32 +0200

    exercise 41

--- 41.access-array-data-with-indexes.js
+++ 41.access-array-data-with-indexes.js
@@ -1,3 +1,3 @@
-// Modifica los datos de un arreglo con índices
+//Accede a los datos de un arreglo con índices
 const myArray = [50, 60, 70];
 const myData  = myArray[0];
--- 42.modify-array-data-with-indexes.js
+++ 42.modify-array-data-with-indexes.js
@@ -1,5 +1,3 @@
-// Configuración
+// Modifica los datos de un arreglo con índices
 const myArray = [18, 64, 99];
-
-// Cambia solo el código debajo de esta línea
 myArray[0] = 45;
