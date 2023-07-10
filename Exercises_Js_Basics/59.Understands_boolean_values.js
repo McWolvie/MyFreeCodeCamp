fc3b443be8ad91d13d7a8a4c99e1a5385d1a28b3
McWolvie <lobezno2001@gmail.com>
2023-07-10 14:24:25 +0200

    Eejercicios básicos de Js de FreeCodeCamp

---
+++ Exercises_Js_Basics/59.Understands_boolean_values.js
@@ -0,0 +1,15 @@
+// Comprende los valores booleanos
+
+// Otro tipo de datos es el Booleano. Los booleanos solo pueden ser uno de dos valores: true (verdadero) o false (falso). Básicamente son pequeños interruptores de encendido, donde true es encendido y false es apagado. Estos dos estados se excluyen mutuamente.
+
+// Nota Los valores del tipo booleano nunca se escriben con comillas. Las cadenas "true" y "false" no son booleanos y no tienen ningún significado especial en JavaScript.
+
+// Modifica la función welcomeToBooleans para que devuelva true en lugar de false.
+
+function welcomeToBooleans() {
+    // Cambia solo el código debajo de esta línea
+  
+    return true; // Cambia esta línea. El orignal muestra return false;
+  
+    // Cambia solo el código encima de esta línea
+  }
