191e085dbb128e4cdc4ead41bf3bfb00ae9f92c6
McWolvie <lobezno2001@gmail.com>
2023-07-11 14:17:51 +0200

    Eejercicios básicos de Js de FreeCodeCamp

---
+++ Exercises_Js_Basics/67.Comparisons_with_the_logical_operator_or.js
@@ -0,0 +1,37 @@
+// Comparaciones con el operador lógico "or"
+
+// El operador lógico or (||) devuelve true si cualquiera de los operandos es true. De lo contrario, devuelve false.
+
+// El operador lógico or se compone de dos símbolos de barra vertical: (||). Este se puede encontrar normalmente entre las teclas de tabulación y escape.
+
+// El patrón de abajo debe parecer familiar desde los puntos de referencia anteriores.
+
+if (num > 10) {
+  return "No";
+}
+if (num < 5) {
+  return "No";
+}
+return "Yes";
+
+// Este código devolverá Yes si num está entre 5 y 10 (5 y 10 incluidos). La misma lógica se puede escribir con el operador lógico o.
+
+if (num > 10 || num < 5) {
+  return "No";
+}
+return "Yes";
+
+// Combina las dos sentencias if en una sola sentencia que devuelva la cadena Outside si val no está entre 10 y 20, inclusivo. De lo contrario, devuelve la cadena Inside.
+
+function testLogicalOr(val) {
+  // Cambia solo el código debajo de esta línea
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Cambia solo el código encima de esta línea
+  return "Inside";
+}
+
+testLogicalOr(15);
